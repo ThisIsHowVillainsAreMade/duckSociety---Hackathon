83b63755516f22cabab7f4e0fc58034862afc3ee
Océane K <146114113+ThisIsHowVillainsAreMade@users.noreply.github.com>
2024-02-02 00:54:28 +0100

    add a 3D witch

---
+++ src/components/SplineWitch.jsx
@@ -0,0 +1,12 @@
+import "./styles/SplineWitch.css";
+
+import Spline from "@splinetool/react-spline";
+
+export default function SplineWitch() {
+  return (
+    <Spline
+      className="splineCanva"
+      scene="https://prod.spline.design/JK1NenmLuSjqF7kf/scene.splinecode"
+    />
+  );
+}
