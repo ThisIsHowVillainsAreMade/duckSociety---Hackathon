3bb5a1c472fd115f00327ef1a22013061eb8b79b
Florian <f.schaessens@gmail.com>
2024-02-01 19:58:42 +0100

    add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./root.css";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <main>
-      <nav>
-        <Navbar />
-      </nav>
-      <section>
-        <Outlet />
-      </section>
-    </main>
+    <>
+      <Outlet />
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,16 @@
 import "./styles/Navbar.css";
+
+
 function Navbar() {
-  return;
+  return (
+    <nav>
+      <h2 id="logo">DuckSociety</h2>
+      <ul>
+        <li id="menu">Menu</li>
+        <li id="notice">Notice</li>
+        <li id="about">A propos</li>
+      </ul>
+    </nav>
+  )
 }
 export default Navbar;
